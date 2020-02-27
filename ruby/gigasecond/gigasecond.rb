ac3a2df1ed03f09503d78ae619b6fec15cb0a555
Kyle Boehlen <kboehlen@kengarff.com>
2020-02-27 13:30:44 -0700

    Refactor gigasecond to support all logical objs

--- ruby/gigasecond/gigasecond.rb
+++ ruby/gigasecond/gigasecond.rb
@@ -1,17 +1,33 @@
 require 'date' # require for DateTime class
+require 'minitest/autorun' # require for tests
 
 class Gigasecond
-  GIGASECOND =
-  {
-    Seconds: (10**9), # define gigasecond in seconds
-    Days: (10**9).to_f / 86400 # define gigasecond in days
-  }
+  GIGASECOND = 1_000_000_000 # define gigasecond in Integer as number of seconds
 
-  def self.from(time) # accepts Time or DateTime and returns a Time object with a gigasecond added
-    unless time.instance_of? DateTime # check if time is DateTime instead of Time
-      return time + GIGASECOND[:Seconds] # return gigasecond added to time
+  def self.from(time)
+    if [Date, DateTime].any? { |c| time.instance_of? c }  # check if time is Date or DateTime
+      time = time.to_time # Convert time to Time
     end
 
-    (time + GIGASECOND[:Days]).to_time # add days to DateTime obj and return Time obj
+    time + GIGASECOND # Return time with a gigasecond added
+  end
+end
+
+if defined?(Minitest)
+  puts 'Mini test defined, running tests...'
+
+  describe Gigasecond do
+    describe 'Custom Test Based on Name of Class' do
+      it 'must accept the simple thing' do
+        expected = 1000000012.75
+        _(Gigasecond.from(12.75)).must_equal expected
+      end
+
+      it 'must give a correct date if a Date of 1970, 1, 1 is given' do
+        expected = Date.new(2001, 9, 9)
+        actual = Gigasecond.from(Date.new(1970, 1, 1)).to_date
+        _(actual).must_equal expected
+      end
+    end
   end
 end
