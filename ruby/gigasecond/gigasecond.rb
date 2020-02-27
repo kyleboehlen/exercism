ca78386476aa14799a83b85a3c2b3819cab3da79
Kyle Boehlen <kboehlen@kengarff.com>
2020-02-27 16:59:31 -0700

    Gigasecond refactor to fit the duck typing principle

--- ruby/gigasecond/gigasecond.rb
+++ ruby/gigasecond/gigasecond.rb
@@ -1,21 +1,15 @@
 require 'date' # require for DateTime class
-require 'minitest/autorun' # require for tests
 
 class Gigasecond
   GIGASECOND = 1_000_000_000 # define gigasecond in Integer as number of seconds
+  GIGASECOND_AS_DAYS = Rational(GIGASECOND, 86400) # define gigasecond in Rational as number of days
 
-  def self.from(time)
-    if [Date, DateTime].any? { |c| time.instance_of? c }  # check if time is Date or DateTime
-      time = time.to_time # Convert time to Time
-    end
-
-    time + GIGASECOND # Return time with a gigasecond added
+  def self.from(time) # Accepts Numeric, DateTime, and Time
+    time + (([Date, DateTime].any? { |c| time.instance_of? c }) ? GIGASECOND_AS_DAYS : GIGASECOND) # Will return whatever type was passed with gigasecond added
   end
 end
 
 if defined?(Minitest)
-  puts 'Mini test defined, running tests...'
-
   describe Gigasecond do
     describe 'Custom Test Based on Name of Class' do
       it 'must accept the simple thing' do
@@ -23,10 +17,34 @@ if defined?(Minitest)
         _(Gigasecond.from(12.75)).must_equal expected
       end
 
-      it 'must give a correct date if a Date of 1970, 1, 1 is given' do
-        expected = Date.new(2001, 9, 9)
-        actual = Gigasecond.from(Date.new(1970, 1, 1)).to_date
-        _(actual).must_equal expected
+      it 'must result in a Integer object when a integer is given' do
+        expected = Integer
+        actual = Gigasecond.from(12)
+        _(actual).must_be_kind_of Integer
+      end
+
+      it 'must result in a Float object when a float is given' do
+        expected = Float
+        actual = Gigasecond.from(12.75)
+        _(actual).must_be_kind_of Numeric
+      end
+
+      it 'must result in a DateTime object when a DateTime is given' do
+        expected = DateTime
+        actual = Gigasecond.from(DateTime.new(1970, 1, 1, 0, 0, 0))
+        _(actual).must_be_kind_of Date
+      end
+
+      it 'must result in a Date object when a date is given' do
+        expected = Date
+        actual = Gigasecond.from(Date.new(1970, 1, 1))
+        _(actual).must_be_kind_of Date
+      end
+
+      it 'must result in a Time object when time is given' do
+        expected = Time
+        actual = Gigasecond.from(Time.now)
+        _(actual).must_be_kind_of Time
       end
     end
   end
--- ruby/gigasecond/gigasecond_test.rb
+++ ruby/gigasecond/gigasecond_test.rb
@@ -5,16 +5,17 @@ require_relative 'gigasecond'
 # Common test data version: 1.1.0 5506bac
 class GigasecondTest < Minitest::Test
   def test_date_only_specification_of_time
-    # skip
     assert_equal Time.utc(2043, 1, 1, 1, 46, 40), Gigasecond.from(Time.utc(2011, 4, 25, 0, 0, 0))
 
     # test for gigasecond by passing DateTime obj
-    assert_equal DateTime.new(2043, 1, 1, 1, 46, 40).to_time.to_i, Gigasecond.from(DateTime.new(2011, 4, 25, 0, 0, 0)).to_i
+    assert_equal DateTime.new(2043, 1, 1, 1, 46, 40), Gigasecond.from(DateTime.new(2011, 4, 25, 0, 0, 0))
   end
 
   def test_second_test_for_date_only_specification_of_time
-    skip
     assert_equal Time.utc(2009, 2, 19, 1, 46, 40), Gigasecond.from(Time.utc(1977, 6, 13, 0, 0, 0))
+
+    # test for gigasecond by passing DateTime obj
+    assert_equal DateTime.new(2009, 2, 19, 1, 46, 40), Gigasecond.from(DateTime.new(1977, 6, 13, 0, 0, 0))
   end
 
   def test_third_test_for_date_only_specification_of_time
