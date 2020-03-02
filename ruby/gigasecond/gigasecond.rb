daaf1609a44f16224d1201b86af0a090ff961b1f
Kyle Boehlen <kboehlen@kengarff.com>
2020-03-02 14:32:30 -0700

    Refactor Gigasecond, add Second class for testing

--- ruby/gigasecond/gigasecond.rb
+++ ruby/gigasecond/gigasecond.rb
@@ -1,22 +1,16 @@
-require 'date' # for DateTime
+require 'date'
 
 class Gigasecond
   GIGASECOND = 1_000_000_000
-  AS_DAYS = Rational(GIGASECOND, 86400)
-  private_constant :AS_DAYS
 
-  # Accepts Numeric, DateTime, and Time
+  # Accepts base unit seconds
   def self.from(seconds)
-    case seconds
-    when Numeric, Time
-      seconds + GIGASECOND
-    when DateTime
-      seconds + AS_DAYS
-    else
-      raise ArgumentError.new(
-        'Invalid class %s given' % seconds.class
-      )
-    end
+    # Actively guard against base unit of days
+    raise ArgumentError.new(
+      'Invalid class %s given' % seconds.class
+    ) if [Date, DateTime].any? { |c| seconds.instance_of? c }
+
+    seconds + GIGASECOND
   end
 end
 
@@ -43,12 +37,6 @@ if defined?(Minitest)
         _(actual).must_be_kind_of Numeric
       end
 
-      it 'must result in a DateTime object when a DateTime is given' do
-        expected = DateTime
-        actual = Gigasecond.from(DateTime.new(1970, 1, 1, 0, 0, 0))
-        _(actual).must_be_kind_of Date
-      end
-
       it 'must result in a Time object when time is given' do
         expected = Time
         actual = Gigasecond.from(Time.now)
@@ -58,6 +46,12 @@ if defined?(Minitest)
       it 'must raise error ArgumentError if Date is given' do
         assert_raises ArgumentError do
           _(Gigasecond.from(Date.new(1900, 1, 1)))
+        end
+      end
+
+      it 'must raise error ArguementError DateTime is given' do
+        assert_raises ArgumentError do
+          _(Gigasecond.from(DateTime.new(1970, 1, 1, 0, 0, 0)))
         end
       end
     end
---
+++ ruby/gigasecond/seconds.rb
@@ -0,0 +1,27 @@
+class Seconds
+
+  private
+  def initialize(number)
+    @seconds = number
+  end
+
+  attr_reader :seconds
+
+  public
+
+  def +(other)
+    Seconds.new(seconds + other)
+  end
+  def to_i
+    seconds.to_i
+  end
+  def to_f
+    seconds.to_f
+  end
+  def to_s
+    '%f' %  seconds
+  end
+  def to_r
+    seconds.to_r
+  end
+end
